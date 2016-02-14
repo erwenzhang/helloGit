f7e0f8d3b7a8475368e37c8db1dec4299a54d9a5
erwenzhang <zhangvivien@126.com>
2016-02-14 10:21:58 -0600

    hashmap,

---
+++ leetCode/1TwoSum.java
@@ -0,0 +1,17 @@
+public class Solution {
+   public int[] twoSum(int[] nums,int target){
+    	int[] index = new int[2];
+	Map<Integer,Integer> map = new HashMap<>();
+     	for(int i = 0;i<nums.length;i++){
+    	    if(map.containsKey(nums[i])){
+    		index[0] = map.get(nums[i]);
+    		index[1] = i;
+    		return index;	
+            }else{
+            	map.put(target-nums[i],i);
+            }
+        }
+	    return index;
+    }
+    
+}
