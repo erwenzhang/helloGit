c1c035a40e8bfb0c4ba369742a9910627f3508bb
erwenzhang <zhangvivien@126.com>
2016-02-21 15:28:30 -0600

    linkedlist,

---
+++ AmazonOA/CacheMiss.java
@@ -0,0 +1,21 @@
+public class CacheMiss{
+    public int Solution(int[] key,int size){
+        if(key==null||key.length==0) return 0;
+        List<Integer> cache = new LinkedList<Integer>();
+        int count = 0;
+        for(int i=0;i<key.length;i++){
+            if(cache.contains(key[i]))
+                cache.remove(new Integer(key[i]));
+            else{
+                  count++;
+                  if(cache.size()==size){
+                    cache.remove(0);
+                  }
+            }
+            cache.add(key[i]);
+        }
+        return count;
+        
+    }
+    
+}
