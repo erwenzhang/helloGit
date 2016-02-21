801e598287590f0310f3c8cc7648b461fe12cdc8
erwenzhang <zhangvivien@126.com>
2016-02-21 17:40:00 -0600

    instead of using hashset, use two or three pointers

---
+++ AmazonOA/LinkedListLoop.java
@@ -0,0 +1,32 @@
+public class LinkedListLoop{
+    public boolean hasCycle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+    public ListNode detectCycle(ListNode head){
+        if(head==null||head.next==null) return null;
+        ListNode slow =head;
+        ListNode fast = head;
+        ListNode entry = head;
+        
+        while(fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow = fast){
+                while(entry!=slow){
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+    
+}
