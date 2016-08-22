9ae380fd9d3e9b1127058300dff45e670fddfdfe
erwenzhang <zhangvivien@126.com>
2016-08-22 16:27:53 -0500

    Update 323numberOfConnectedComponetsInAUndirectedGraph.java

--- leetCode/323numberOfConnectedComponetsInAUndirectedGraph.java
+++ leetCode/323numberOfConnectedComponetsInAUndirectedGraph.java
@@ -1,46 +1,37 @@
 public class Solution {
-      public int countComponents(int n, int[][] edges){
-        boolean[] visitNodes = new boolean[n];
-        if(n==0||edges==null){
-            return 0;
+    public int countComponents(int n, int[][] edges){
+        Set<Integer> visited = new HashSet<Integer>();    
+        Graph gra = new Graph(edges,n);
+        int components = 0;
+        for(int i = 0;i<n;i++){
+            if(visited.add(i)){
+                dfs(gra,i,visited);
+                components++;
+            }     
         }
-        Graph gra = new Graph(edges);
-        int numOfComponents = 0;
-        for(int i = 0 ;i < n;i++){
-            if(visitNodes[i]){
-                continue;
-            }
-            traverse(gra,i,visitNodes);
-            numOfComponents++;
-        }
-        return numOfComponents;
-    }
-    public void traverse(Graph graph,int i,boolean[] visitNodes){
-        visitNodes[i] = true;
-        Set<Integer> vertexSet = graph.edges.get(i);
+         return components;
+     }
+
+    public void dfs(Graph graph,int i,boolean[] visitNodes){
+        Set<Integer> vertexSet = graph.get(i);
         if(vertexSet == null) return;
         for(Integer vertex: vertexSet){
-            if(visitNodes[vertex]){ 
-                continue;
+            if(visiteNodes.add(vertex)){
+                dfs(graph,vertex,visitNodes);
             }
-            traverse(graph,vertex,visitNodes);
         }
     }
+
     public static class Graph{
-        Map<Integer,Set<Integer>> edges = new HashMap<Integer,Set<Integer>>();
-        Graph(int[][] edges){
+        Map<Integer,Set<Integer>>[] edges = new     HashSet<Integer,Set<Integer>>();
+        Graph(int[][] edges, int n){
+            for(int i = 0;i<n;i++){
+                edges.put(i,new HashSet<Integer>());
+            }  
             for(int[] edge:edges){
-                Set<Integer> vertexSet0 = this.edges.get(edge[0]);
-                Set<Integer> vertexSet1 = this.edges.get(edge[1]);
-                if(vertexSet0 == null){
-                    this.edges.put(edge[0], new HashSet<Integer>());
-                }
-                if(vertexSet1 == null){
-                    this.edges.put(edge[1], new HashSet<Integer>());
-                }
-                this.edges.get(edge[0]).add(edge[1]);
-                this.edges.get(edge[1]).add(edge[0]);
-            }
-        }
+                graph.get(edge[0]).add(edge[0]);
+                graph.get(edge[1]).add(edge[1]);
+            } 
+         } 
     }
 }
