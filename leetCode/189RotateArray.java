f6bdfa9839fe704c44b493e2bf74c9a4be4457e4
erwenzhang <zhangvivien@126.com>
2016-09-11 17:22:47 -0500

    Three reverse,

---
+++ leetCode/189RotateArray.java
@@ -0,0 +1,17 @@
+public class Solution {
+     public void rotate(int[] nums, int k){
+         k = k%nums.length;
+         reverse(0,nums.length-1,nums);
+         reverse(0,k-1,nums);
+         reverse(k,nums.length-1,nums);
+    }
+    public void reverse(int i,int j, int[] nums){
+        while(i<j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+}
