b1544e3256b41baa4c651bc36fefff296b616c34
erwenzhang <zhangvivien@126.com>
2016-09-13 20:46:44 -0500

    Update 46Permutations.java

--- leetCode/46Permutations.java
+++ leetCode/46Permutations.java
@@ -1,27 +1,27 @@
 public class Solution {
     public List<List<Integer>> permute(int[] array){
 	    if(array==null||array.length == 0) return null;
-        int len = array.length;
+            int len = array.length;
 	    List<List<Integer>> permutations = new ArrayList<>();
 	    List<Integer> permutation = new ArrayList<>();
-	    boolean[] flag = new boolean[len];
-	    generate(permutations,array,0,permutation,flag);	
+	    boolean[] visited = new boolean[len];
+	    generate(permutations,array,0,permutation,visited);	
 	    return permutations;
 
     }
-    public void generate(List<List<Integer>> permutations,int[] array,int index,List<Integer> permutation,boolean[] flag){
+    public void generate(List<List<Integer>> permutations,int[] array,int index,List<Integer> permutation,boolean[] visited){
     	if(index==array.length){
             List<Integer> tmp = new ArrayList<>(permutation);
             permutations.add(tmp);
             return;
         }
     	for(int i = 0;i<array.length;i++){
-    	    if(flag[i]) continue;
-          permutation.add(array[i]);
-          flag[i] = true;
-    	    generate(permutations,array,index+1,permutation,flag);
+    	    if(visited[i]) continue;
+            permutation.add(array[i]);
+            visited[i] = true;
+    	    generate(permutations,array,index+1,permutation,visited);
     	    permutation.remove(index);
-    	    flag[i] = false;
+    	    visited[i] = false;
         }
     }
 }
