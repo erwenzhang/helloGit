25a6d44f232c636c4912a8c5033a264d3fe6037f
erwenzhang <zhangvivien@126.com>
2016-09-15 20:37:01 -0500

    be very careful with k, k/2, k-k/2

--- leetCode/4MedianOfTwoSortedArrays.java
+++ leetCode/4MedianOfTwoSortedArrays.java
@@ -1,10 +1,10 @@
 public float findMedianSortedArray(int[] array1, int[] array2){ 
   int total = array1.length+array2.length;
-  if(total%2==1) return findKthElement(array1,0,array1.length-1,array2, 0, array2.length-1,total/2+1);
+  if(total%2 == 1) return findKthElement(array1, 0, array2, 0, total/2+1);
   else{
-      float mid1 = findKthElement(array1,0, array1.length-1,array2,0,array2.length-1,total/2);
+      float mid1 = findKthElement(array1, 0, array2, 0, total/2);
       float mid2 = findKthElement(array1, 0, array2, 0, total/2+1);
-      return (mid1+mid2)/2/0;
+      return (mid1+mid2)/2.0;
   } 
 }
 
@@ -16,8 +16,8 @@ public float findKthElement(int[] array1, int lo1, int[] array2, int lo2, int k)
    int kth1 = (lo1+k/2-1)<array1.length?array1[lo1+k/2-1]:Integer.MAX_VALUE;
    int kth2 = (lo2+k/2-1)<array2.length?array2[lo2+k/2-1]:Integer.MAX_VALUE;
    if(kth1<kth2){
-       return findKthElement(array1, lo1+k/2, array2, lo2, k- k/2);
+       return findKthElement(array1, lo1+k/2, array2, lo2, k - k/2);
    }else{
-       return findKthElement(array1, lo1, array2, lo2+k/2, k-  k/2);
+       return findKthElement(array1, lo1, array2, lo2+k/2, k -  k/2);
    }  
 }
