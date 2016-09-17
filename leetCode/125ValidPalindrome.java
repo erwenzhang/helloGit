e255abd068f84fefec409d87935a8788959366a0
erwenzhang <zhangvivien@126.com>
2016-09-17 17:56:37 -0500

    two pointers, Character.isLetterOrDigit(char),  Character.toLowerCase(char)

--- leetCode/125ValidPalindrome.java
+++ leetCode/125ValidPalindrome.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean isPalindrome(String s) {
         int i = 0,j=s.length()-1;
         while(i<j){
-        	while(!(Character.isLetterOrDigit(s.charAt(j)))){
+            while(!(Character.isLetterOrDigit(s.charAt(j)))){
         		  j--;
             }
             while(!(Character.isLetterOrDigit(s.charAt(i)))){
