0f424466d2e4d5a56c55a1d12c89d7b0bb6abf32
erwenzhang <zhangvivien@126.com>
2016-09-17 15:21:12 -0500

    slow, but clear

--- leetCode/146LRUCacheSlow.java
+++ leetCode/146LRUCacheSlow.java
@@ -2,24 +2,23 @@ import java.util.HashMap;
 
 public class LRUCache{
     static class Node{
-        public final int key;
-        public int val;
-        public Node left;
-        public Node right;
+        int key;
+        int val;
+        Node left;
+        Node right;
         Node(int k, int v){
             key = k;
             val = v;
         }
     }
     int capacity;
-    Node p;
-    Node dummyHead;
+    Node head;
     HashMap<Integer, Node> cache;
     public LRUCache(int capacity){
         this.capacity = capacity;
         cache = new HashMap<>();
-        p = new Node(-1,-1);
-        dummyHead = p;
+        head = new Node(-1,-1);
+        head.right = head.left = head;
     }
 
     public int get(int key){
@@ -27,7 +26,6 @@ public class LRUCache{
         replace(key);
         return cache.get(key).val;
     }
-    
     public void set(int key, int value){
         if(capacity == 0) return;
         if(cache.containsKey(key)){
@@ -45,27 +43,22 @@ public class LRUCache{
 
     public void replace(int key){
         Node cur = cache.get(key);
-        if(cur == p) return;
         cur.left.right = cur.right;
         cur.right.left = cur.left;
         addLRU(cur);
     }
     
     public void addLRU(Node node){
-        p.right = node;
-        node.left = p;
-        node.right = null;
-        p = node;
+        node.right = head;
+        node.left = head.left;
+        head.left.right = node;
+        head.left = node;
     }
 
     public void removeLRU(){
-        int key = dummyHead.right.key;
+        int key = head.right.key;
         cache.remove(key);
-        if(dummyHead.right == p){
-            p = dummyHead;
-        }else{
-            dummyHead.right.right.left = dummyHead;
-        }
-        dummyHead.right = dummyHead.right.right;
+        head.right.right.left = head;
+        head.right = head.right.right;
     }
 }
