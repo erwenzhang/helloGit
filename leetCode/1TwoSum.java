5d4425917695847528a233bc1b6ced09a3bd8cbf
erwenzhang <zhangvivien@126.com>
2016-09-17 17:21:47 -0500

    throw IllegalArgumentException("No solution.")

--- leetCode/1TwoSum.java
+++ leetCode/1TwoSum.java
@@ -11,25 +11,6 @@ public class Solution {
             	map.put(target-nums[i],i);
             }
         }
-	    return index;
+	    throw IllegalArgumentException("No two sum solution.");
     }
-    
 }
-
-public class Solution{
-  public int[] twoSum(int[] nums, int target){
-     Map<Integer,Integer> map = new HashMap<>();
-     int[] result = new int[2];
-     for(int i = 0;i<nums.length;i++){
-       if(map.get(nums[i])!=null){
-          result[0]=map.get(nums[i]);
-          result[1]=i;
-          return result;
-       }
-       map.put(target-nums[i],i);
-     }
-     return result;
-  }
-  
-}
-
