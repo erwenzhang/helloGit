d5ed03fc1fb101266f728a7348fd08169fb534b2
erwenzhang <zhangvivien@126.com>
2016-09-19 16:52:07 -0500

    make s is <= t, simplify logics inside while loop

--- leetCode/161OneEditDistance.java
+++ leetCode/161OneEditDistance.java
@@ -1,25 +1,26 @@
 public class Solution {
     public boolean isOneEditDistance(String s, String t) {
-        if(Math.abs(s.length()-t.length())>1) return false; 
-        int len1 = s.length();
-        int len2 = t.length();
-        int p1 = 0;
-        int p2 = 0;
-        int count = 0;
-        while(p1<len1&&p2<len2){
-        	if(s.charAt(p1)!=t.charAt(p2)){
-	            count++;
-	            if(count>1) return false;
-              if((p1+1<len1)&&s.charAt(p1+1)==t.charAt(p2)){
-                    p1++;
-              }else if((p2+1<len2)&&t.charAt(p2+1)==s.charAt(p1)){
-                	p2++;
-              }
-          }
-          p1++;
-          p2++;
-        } 
-        if(p1<len1||p2<len2) return count==0;
-        else return count!=0;
+        int m = s.length();
+        int n = t.length();
+        if(m > n) return isOneEditDistance(t, s);
+        if(n - m > 1) return false;
+        int i = 0;
+        int j = 0;
+        int diff = 0;
+        boolean sameLen = m==n?true:false;
+        while(i<m&&j<n){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++; 
+                continue; 
+            }
+            diff++;
+            if(diff > 1) return false;
+            j++;
+            if(sameLen){
+                i++;
+            }
+        }
+        return i==m&&j==n&&diff==1||j==n-1&&diff==0;
     }
 }
