7d93e97bd33802b647b10d076a4172bbcd74f655
erwenzhang <zhangvivien@126.com>
2016-09-23 15:01:31 -0500

    Top down, Bottom Up

--- leetCode/156BinaryTreeUpsideDown.java
+++ leetCode/156BinaryTreeUpsideDown.java
@@ -1,3 +1,4 @@
+/*Top Down*/
 public class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root){
         TreeNode p = root, parentRight = null, parent = null;
@@ -11,4 +12,18 @@ public class Solution {
         }
         return parent;
    }
+}
+
+/*Bottom Up*/
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root){
+        return dfs(root, null);
+    }
+    public TreeNode dfs(TreeNode left, TreeNode parent){
+        if(left == null) return parent;
+        TreeNode root = dfs(left.left, left);
+        left.left = (parent==null)?parent:parent.right;
+        left.right = parent;
+        return root	;
+    }
 }
