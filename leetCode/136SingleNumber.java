5093bb0ed357aaad6fd17efb0d2b3a45720b659d
erwenzhang <zhangvivien@126.com>
2016-09-23 22:20:30 -0500

    HashSet, XOR

---
+++ leetCode/136SingleNumber.java
@@ -0,0 +1,19 @@
+public int singleNumber(int[] A) {
+    Set<Integer> set = new HashSet<>();
+    for (int x : A) {
+        if (set.contains(x)) {
+            set.remove(x);
+        } else {
+            set.add(x);
+        }
+    }
+    return set.iterator().next();
+}
+
+public int singleNumber(int[] A) {
+    int num = 0;
+    for (int x : A) {
+      num ^= x;
+    }
+    return num;
+}
