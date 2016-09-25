167fdba6e26f92589c49abbd159297520b94e29b
erwenzhang <zhangvivien@126.com>
2016-09-25 17:16:44 -0500

    backtracking, dfs, recursion

--- leetCode/17LetterCombination.java
+++ leetCode/17LetterCombination.java
@@ -1,42 +1,37 @@
 public class Solution {
-    public List<String> letterCombinations(String digits){
-    	if(digits==null||digits.length()==0)
-    		return new ArrayList<>();
-    	else{
-    	    List<String> rets = new ArrayList<>();
-    	    StringBuilder sb = new StringBuilder();
-    	    dfs(rets,sb,0,digits);
-    	    return rets;
-    	}
-    }
-    public void dfs(List<String> rets,StringBuilder sb,int index,String digits){
-    	if(index==digits.length()){
-    	        rets.add(sb.toString());
-    	        return;
-      }
-    	int cur = digits.charAt(index)-'0';
-    	if(cur==0||cur==1) return;
-        int start;
-        switch(cur){
-            case 7:
-                start = 'p';
-                break;
-            case 8:
-                start = 't';
-                break;
-            case 9:
-                start = 'w';
-                break;
-            default:
-                start = (cur-2)*3+'a';
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        Map<Character, char[]> map = new HashMap<Character, char[]>();
+        if (digits == null || digits.equals("")) {
+            return combinations;
         }
-    	int count=(cur==9||cur==7)?4:3;
-    	int len = sb.length();
-    	for(int i=0;i<count;i++){
-    		sb.append((char)(start+i));
-    		dfs(rets,sb,index+1,digits);
-    		sb.setLength(len);
-      }
+        map.put('0', new char[] {});
+        map.put('1', new char[] {});
+        map.put('2', new char[] { 'a', 'b', 'c' });
+        map.put('3', new char[] { 'd', 'e', 'f' });
+        map.put('4', new char[] { 'g', 'h', 'i' });
+        map.put('5', new char[] { 'j', 'k', 'l' });
+        map.put('6', new char[] { 'm', 'n', 'o' });
+        map.put('7', new char[] { 'p', 'q', 'r', 's' });
+        map.put('8', new char[] { 't', 'u', 'v'});
+        map.put('9', new char[] { 'w', 'x', 'y', 'z' });
+        StringBuilder sb = new StringBuilder();
+        helper(map, sb, combinations,digits);
+        return combinations;
     }
 
+    public void helper(Map<Character, char[]> map, StringBuilder sb, List<String> combinations, String digits){
+        if(digits.length()== sb.length()){
+            combinations.add(sb.toString());
+            return;
+        }
+        int index = sb.length();
+        char[] array =  map.get(digits.charAt(index));
+   
+        for(char item: array){
+            sb.append(item);
+            helper(map, sb, combinations, digits);
+            sb.setLength(index);
+        } 
+    }
 }
