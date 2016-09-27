cfb7fc4392f778da4aeb6677759929d8696ef5cc
erwenzhang <zhangvivien@126.com>
2016-09-27 10:24:27 -0500

    Update 27RemoveElement.java

--- leetCode/27RemoveElement.java
+++ leetCode/27RemoveElement.java
@@ -1,5 +1,38 @@
 public class Solution {
+    /*1*/
     public int removeElement(int[] nums, int val) {
+      int nonVal = 0;
+      int valIn = 0;
+      while(nonVal<nums.length){
+          if(nums[valIn]!=val){
+              valIn++;
+              nonVal = valIn;
+              continue;
+          }
+          if(nums[nonVal]==val){
+              nonVal++;
+              continue;
+          }
+        nums[valIn] = nums[nonVal];
+        nums[nonVal] = val;
+        valIn++;
+        nonVal++;
+    } 
+    return valIn;
+}
+    /*2*/
+public int removeElement(int[] A, int elem) {
+  int m = 0;    
+  for(int i = 0; i < A.length; i++){
+      if(A[i] != elem){
+          A[m] = A[i];
+          m++;
+      }
+  }
+  return m;
+}
+/*3*/
+      public int removeElement(int[] nums, int val) {
         int p1 = 0;
         int p2 = nums.length-1;
         while(p1<=p2){
