73b2cccd4bf333b2a667fa4fdfbd222d60d39d1e
erwenzhang <zhangvivien@126.com>
2016-09-29 11:58:56 -0500

    Update 357CountNumbersWithUniqueDigits.java

--- leetCode/357CountNumbersWithUniqueDigits.java
+++ leetCode/357CountNumbersWithUniqueDigits.java
@@ -15,3 +15,22 @@ public class Solution {
         return sum;     
     }
 }
+
+
+
+//Simpler
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n){
+       if(n==0) return 1;
+       if(n>10) n = 10;
+       int res = 10;
+       int availableNums = 9;
+       int uniqueDigits = 9;
+       for(int i = 2; i <= n; i++){
+           uniqueDigits = availableNums*uniqueDigits;
+           res += uniqueDigits;
+           availableNums--;  
+       }
+       return res;
+    }
+}
