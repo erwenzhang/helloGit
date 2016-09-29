267ed52474f2ca2a5788775bf869df3977a67356
erwenzhang <zhangvivien@126.com>
2016-09-29 11:37:59 -0500

    f[i] i digits with 0, g[i] i digits without 0

---
+++ leetCode/357CountNumbersWithUniqueDigits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n == 0) return 1;
+        if(n>10) n = 10;
+        int[] f = new int[11];
+        int[] g = new int[11];
+        g[1] = 9;
+        f[1] = 1;
+        int sum = g[1]+f[1];
+        for(int i = 2; i <= n; i++){
+            f[i] = f[i-1]*(10-(i-1)) + g[i-2]*(10-(i-1));
+            g[i] = g[i-1]*(9-(i-1));
+            sum = sum + f[i] + g[i];
+        }  
+        return sum;     
+    }
+}
