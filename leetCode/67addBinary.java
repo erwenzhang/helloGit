442691c2a3d63d6faf273e4edbe619f0622c4022
erwenzhang <zhangvivien@126.com>
2016-09-29 21:38:58 -0500

    Update 67addBinary.java

--- leetCode/67addBinary.java
+++ leetCode/67addBinary.java
@@ -27,3 +27,19 @@ public String addBinary(String s1, String s2){
     
         return res;
 }
+
+public String addBinary(String a, String b){
+    int carry = 0;
+    int p1 = a.length()-1;
+    int p2 = b.length()-1;
+    StringBuilder sb = new StringBuilder();
+    while(p1>=0||p2>=0){
+        int add1 = (p1<0)?0:a.charAt(p1--)-'0';
+        int add2 = (p2<0)?0:b.charAt(p2--)-'0';
+        int sum = add1+add2+carry;
+        carry = sum/2;
+        sb.append(sum%2);
+    }
+    if(carry>0) sb.append(carry);
+    return sb.reverse().toString();
+ }
