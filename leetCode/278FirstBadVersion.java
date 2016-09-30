ef66f84b6132ee2c1af92546a85caa949ecfbeea
erwenzhang <zhangvivien@126.com>
2016-09-30 10:52:30 -0500

    binary search, lo = mid + 1

--- leetCode/278FirstBadVersion.java
+++ leetCode/278FirstBadVersion.java
@@ -6,16 +6,13 @@ public class Solution extends VersionControl {
     	int lo = 1;
     	int hi = n;
     	while(lo<hi){
-    		int mid = lo+(hi-lo)/2;
-    		if(isBadVersion(mid)){
-    			hi = mid;
-        }else{
-    	    lo = mid+1;
-        }
+    	    int mid = lo+(hi-lo)/2;
+    	    if(isBadVersion(mid)){
+    	        hi = mid;
+          }else{
+    	        lo = mid+1;
+          }
       }
       return lo;
     }
-
-
-
 }
