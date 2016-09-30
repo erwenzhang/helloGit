d772822b861fbf85887c8a6c90b40f132c9fdad4
erwenzhang <zhangvivien@126.com>
2016-09-29 21:14:44 -0500

    Solution2, without sorting, using HashMap,

--- leetCode/15 ThreeSum.java
+++ leetCode/15 ThreeSum.java
@@ -22,3 +22,32 @@ public List<List<Integer>> threeSum(int[] nums){
     }
   return result;
 }
+
+// Solution2, without sorting, using HashMap,
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums==null||nums.length<3) return res;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.containsKey(num)?map.get(num)+1:1);
+        }
+        HashSet<Integer> set = new HashSet<>(map.keySet());
+        for(int item : set){
+            map.put(item, map.get(item)-1);
+            if(map.get(item)==0) map.remove(item);
+            helper(res, -item, map);
+            map.remove(item);
+        }
+        return res;
+    }
+    public void helper(List<List<Integer>> res, int target,HashMap<Integer,Integer> map){
+        Set<Integer> visited = new HashSet<>();
+        for(int item:map.keySet()){
+            visited.add(item);
+            if(item== target - item&&map.get(item)>=2||item!=target-item&&map.containsKey(target-item)&&!visited.contains(target-item)){
+                res.add(Arrays.asList(-target,item, target - item));
+            }
+        }
+     }
+}
