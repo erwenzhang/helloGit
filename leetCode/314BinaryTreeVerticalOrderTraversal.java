ddca5706cbfab6f63df052bc3d0997dbfd902794
erwenzhang <zhangvivien@126.com>
2016-10-08 22:05:38 -0500

    Update 314BinaryTreeVerticalOrderTraversal.java

--- leetCode/314BinaryTreeVerticalOrderTraversal.java
+++ leetCode/314BinaryTreeVerticalOrderTraversal.java
@@ -1,14 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public List<List<Integer>> verticalOrder(TreeNode root) {
+/*Sol1, BFS, TreeMap, two loops*/
+
+public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> rets = new ArrayList<>();
+        Map<Integer,List<Integer>> map = new TreeMap<>();
+        if(root==null) return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> levels = new LinkedList<>();
+        queue.add(root);
+        levels.add(0);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            int level = levels.poll();
+            if(!map.containsKey(level)){
+                List<Integer> ret = new ArrayList<Integer>();
+                map.put(level, ret);
+            }
+            map.get(level).add(node.val);
+            if(node.left!=null){
+                queue.add(node.left); levels.add(level-1);
+            }
+            if(node.right!=null){
+                queue.add(node.right); levels.add(level+1);
+            }
+        }
+        for(Map.Entry<Integer,List<Integer>> entry : map.entrySet()){
+            rets.add(entry.getValue());
+        }
+        return rets;
+    }
+
+/*Optimal, Sol2, BFS, HashMap, one loop*/
+public List<List<Integer>> verticalOrder(TreeNode root) {
         LinkedList<List<Integer>> rets = new LinkedList<>();
         if(root==null) return rets;
         Map<Integer,List<Integer>> m = new HashMap<>();
@@ -16,23 +38,17 @@ public class Solution {
         q.add(root);
         q.add(0);
         while(!q.isEmpty()){
-    	    int size = q.size();
-    	    for(int i = 0;i<size;i+=2){
-    	        TreeNode node = (TreeNode)q.poll();
-    	        int index = (Integer)q.poll();
-    	        
-    	        if(m.get(index)==null){ 
+    	    TreeNode node = (TreeNode)q.poll();
+    	    int index = (Integer)q.poll();
+    	    if(m.get(index)==null){ 
     	            LinkedList tmp = new LinkedList<>();
     	            m.put(index,tmp);
     	            if(index>0) rets.add(tmp); // very clever
     	            else rets.addFirst(tmp); 
-    	        }
-              m.get(index).add(node.val);
-    	        if(node.left!=null) {q.add(node.left);q.add(index-1);}
-    	        if(node.right!=null){ q.add(node.right);q.add(index+1);}
-    	        
     	    }
+            m.get(index).add(node.val);
+    	    if(node.left!=null) {q.add(node.left);q.add(index-1);}
+    	    if(node.right!=null){ q.add(node.right);q.add(index+1);}
         }
         return rets;
-    }
-}
+ }
