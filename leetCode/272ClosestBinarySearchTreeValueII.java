33cd388a5be19d566828d9caf941c4173fc1ef86
erwenzhang <zhangvivien@126.com>
2016-10-16 09:32:12 -0700

    Create 272ClosestBinarySearchTreeValueII.java

---
+++ leetCode/272ClosestBinarySearchTreeValueII.java
@@ -0,0 +1,143 @@
+public List<Integer> closestKValues(TreeNode root, double target, int k) {
+  List<Integer> res = new ArrayList<>();
+
+  Stack<Integer> s1 = new Stack<>(); // predecessors, store value less than target
+  Stack<Integer> s2 = new Stack<>(); // successors, store value larger than target
+
+  inorder(root, target, false, s1);
+  inorder(root, target, true, s2);
+  
+  while (k-- > 0) {
+    if (s1.isEmpty())
+      res.add(s2.pop());
+    else if (s2.isEmpty())
+      res.add(s1.pop());
+    else if (Math.abs(s1.peek() - target) < Math.abs(s2.peek() - target))
+      res.add(s1.pop());
+    else
+      res.add(s2.pop());
+  }  
+  return res;
+}
+
+// inorder traversal
+public void inorder(TreeNode root, double target, boolean reverse, Stack<Integer> stack) {
+  if (root == null) return;
+
+  inorder(reverse ? root.right : root.left, target, reverse, stack);
+  // early terminate, no need to traverse the whole tree
+  if ((reverse && root.val <= target) || (!reverse && root.val > target)) return;
+  // track the value of current node
+  stack.push(root.val);
+  inorder(reverse ? root.left : root.right, target, reverse, stack);
+}
+
+
+//o(n), space, o(k);
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        LinkedList<Integer> res = new LinkedList<>();
+        inOrder(root, target, k, res);
+        return res;
+    }
+    
+    private void inOrder(TreeNode root, double target, int k, LinkedList<Integer> res) {
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left, target, k, res);
+        if(res.size() == k) {
+            if(Math.abs(res.get(0) - target) >= Math.abs(root.val - target)) {
+                res.removeFirst();
+                res.add(root.val);
+            } else {
+                return;
+            }
+        } else {
+            res.add(root.val);
+        }
+        inOrder(root.right, target, k, res);
+    }
+}
+
+
+
+//o(logN)
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> ret = new LinkedList<>();
+        Stack<TreeNode> succ = new Stack<>();
+        Stack<TreeNode> pred = new Stack<>();
+        initializePredecessorStack(root, target, pred);
+        initializeSuccessorStack(root, target, succ);
+        if(!succ.isEmpty() && !pred.isEmpty() && succ.peek().val == pred.peek().val) {
+            getNextPredecessor(pred);
+        }
+        while(k-- > 0) {
+            if(succ.isEmpty()) {
+                ret.add(getNextPredecessor(pred));
+            } else if(pred.isEmpty()) {
+                ret.add(getNextSuccessor(succ));
+            } else {
+                double succ_diff = Math.abs((double)succ.peek().val - target);
+                double pred_diff = Math.abs((double)pred.peek().val - target);
+                if(succ_diff < pred_diff) {
+                    ret.add(getNextSuccessor(succ));
+                } else {
+                    ret.add(getNextPredecessor(pred));
+                }
+            }
+        }
+        return ret;
+    }
+
+    private void initializeSuccessorStack(TreeNode root, double target, Stack<TreeNode> succ) {
+        while(root != null) {
+            if(root.val == target) {
+                succ.push(root);
+                break;
+            } else if(root.val > target) {
+                succ.push(root);
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+    }
+
+    private void initializePredecessorStack(TreeNode root, double target, Stack<TreeNode> pred) {
+        while(root != null){
+            if(root.val == target){
+                pred.push(root);
+                break;
+            } else if(root.val < target){
+                pred.push(root);
+                root = root.right;
+            } else{
+                root = root.left;
+            }
+        }
+    }
+    
+    private int getNextSuccessor(Stack<TreeNode> succ) {
+        TreeNode curr = succ.pop();
+        int ret = curr.val;
+        curr = curr.right;
+        while(curr != null) {
+            succ.push(curr);
+            curr = curr.left;
+        }
+        return ret;
+    }
+
+    private int getNextPredecessor(Stack<TreeNode> pred) {
+        TreeNode curr = pred.pop();
+        int ret = curr.val;
+        curr = curr.left;
+        while(curr != null) {
+            pred.push(curr);
+            curr = curr.right;
+        }
+        return ret;
+    }
+}
