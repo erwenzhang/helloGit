c02f66379ce932d2e61f405f592e310b2906e074
erwenzhang <zhangvivien@126.com>
2016-10-23 20:28:03 -0500

    Update and rename 252MeetingRooms to 252MeetingRooms.java

--- leetCode/252MeetingRooms.java
+++ leetCode/252MeetingRooms.java
@@ -11,13 +11,14 @@ public class Solution {
     public boolean canAttendMeetings(Interval[] intervals){
 	    if(intervals==null) return false;
 	    Arrays.sort(intervals,new Comparator<Interval>(){
-            public int compare(Interval a,Interval b){
-                return a.start-b.start;}});
-        for(int i=0;i<intervals.length-1;i++){
+                public int compare(Interval a,Interval b){
+                    return a.start-b.start;
+		}
+            });
+            for(int i=0;i<intervals.length-1;i++){
 	        if(intervals[i].end>intervals[i+1].start)
 		        return false;
-        }
-        return true;
+            }
+            return true;
     }
-
 }
