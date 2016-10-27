7f4ea14605ccbc70228652d1a20bcc83fee5ac39
erwenzhang <zhangvivien@126.com>
2016-10-27 15:24:46 -0700

    Create 268MissingNumber.java

---
+++ leetCode/268MissingNumber.java
@@ -0,0 +1,17 @@
+ public int missingNumber(int[] nums) {
+       int sum = 0;
+       for(int i = 0; i < nums.length;i++){
+          sum+= nums[i];
+       }  
+       int n = nums.length;
+       return n*(n+1)/2-sum;
+    }
+    
+public int missingNumber(int[] nums){
+        int re = nums.length; // very important!!!
+        for(int i = 0; i < nums.length;i++){
+            re^=i;
+            re^=nums[i];
+        }
+        return re;
+    }
